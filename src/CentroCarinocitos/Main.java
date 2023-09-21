f7d1ae4b61355b55f0cca66c5e6ac650c4af6b72
Deiiivy <deivypr28@gmail.com>
2023-09-21 18:30:30 -0500

    Mascotas

---
+++ src/CentroCarinocitos/Main.java
@@ -0,0 +1,23 @@
+package CentroCarinocitos;
+
+public class Main {
+    public static void main(String[] args) {
+        Perro perro1 = new Perro();
+        Perro perro2 = new Perro(10,50,1, "DOG");
+        perro1.setNombre("Firulay");
+        System.out.println("el nombre del perro es "+ perro1.getNombre());
+        System.out.println("el nombre del perro 2 es "+ perro2.getNombre());
+
+        Gato gato1 = new Gato();
+        Gato gato2 = new Gato(10, 50, 1, "CAT");
+        gato1.setNombre("Gatico");
+        System.out.println("El nombre del gato es "+ gato1.getNombre());
+        System.out.println("el nombre del gato 2 es "+ gato2.getNombre());
+
+        Loro loro1 = new Loro();
+        Loro loro2 = new Loro(10, 50, 1, "LORO");
+        loro1.setNombre("Lorito");
+        System.out.println("El nombre del loro es "+ loro1.getNombre());
+        System.out.println("el nombre del loro 2 es "+ loro2.getNombre());
+    }
+}
